496c2fef27ddd9c0a5ba8eb71debe7abd79a66f8
JosephSarpey <josephsarpey2002@gmail.com>
2025-10-16 05:56:53 +0530

    Deployment configuration

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ app.enableCors({
     },
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
